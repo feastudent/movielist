d17dcc2f6c8d0986df0bfc957013a1274baa6528
Francisco E. Alves <fealvesphone@gmail.com>
2021-03-12 13:28:23 -0500

    First commit React Skeleton

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return <h1>React Project Squeleton</h1>
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+console.log("movieList");
